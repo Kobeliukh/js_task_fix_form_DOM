7fbac4b64f29d9bec76d7ee23510bb28c846d138
Vladyslav <vkobeluh@gmail.com>
2025-11-28 12:31:28 +0200

    make build

---
+++ index.a7903e1b.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var a=document.createElement("label");e.placeholder=e.name[0].toUpperCase()+e.name.slice(1),a.className="field-label",a.id=e.id,a.textContent=e.name.toUpperCase(),e.before(a)});
+//# sourceMappingURL=index.a7903e1b.js.map
